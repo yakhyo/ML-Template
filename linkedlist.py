8151eafb324d3462a6b03a618fa2806b758b1a3b
yakhyo <yakhyo9696@gmail.com>
2022-09-08 13:08:58 +0900

    linkedlist.py modified

--- linkedlist.py
+++ linkedlist.py
@@ -6,3 +6,25 @@ class Node:
 
     def __repr__(self):
         return "Node({})".format(self.data)
+
+
+
+def LinkedList:
+
+    def __init__(self, nodes=None):
+        self.head = None
+        if nodes is not None:
+            node = Node(nodes.pop(0))
+            self.head = node
+            for elem in nodes:
+                node.next = Node(elem)
+                node = node.next
+
+    def __repr__(self):
+        nodes = []
+        node = self.head
+        while node:
+            nodes.append(node.data)
+            node = node.next
+        nodes.append("Node")
+        return "->".join([str(node) for node in nodes])
