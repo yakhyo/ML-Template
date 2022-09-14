44e262d162efbe143bf2ca0cc17fd72e326b6870
yakhyo <yakhyo9696@gmail.com>
2022-09-14 16:01:58 +0900

    updates

--- linkedlist.py
+++ linkedlist.py
@@ -1,7 +1,14 @@
 class Node:
-    def __init__(self, data):
+    def __init__(self, data:int):
         self.data = data
         self.next = None
 
     def __repr__(self):
         return f"Node({self.data})"
+
+
+
+
+a = Node()
+
+b = Node("salom")
