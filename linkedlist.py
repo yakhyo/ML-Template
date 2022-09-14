c92f2fb2c9237999940792b76a697709c5de949a
yakhyo <yakhyo9696@gmail.com>
2022-09-14 16:11:28 +0900

    update all

--- linkedlist.py
+++
@@ -1,7 +0,0 @@
-class Node:
-    def __init__(self, data: int):
-        self.data = data
-        self.next = None
-
-    def __repr__(self):
-        return f"Node({self.data})"
--- test.py
+++
@@ -1,5 +0,0 @@
-import random
-
-a = random.randint(0, 100)
-
-print(a)
